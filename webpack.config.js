eec46c3a4ef2c12c73c0a110af75e7f6c38ca1d8
Patrick Sullivan <patrick@jsullivan.org>
2021-05-18 14:11:38 -0700

    Building out backend stuff

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: {
+    src: './client/index.js',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules)/,
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /(node_modules)/,
+        use: [ "style-loader", "css-loader", "sass-loader" ]
+      }
+    ]
+  },
+  devServer: {
+    publicPath: '/build',
+    proxy: {
+      '/api' : 'http://localhost:3000',
+    }
+  }
+}
